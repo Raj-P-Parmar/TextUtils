145ea3c68954cb1ea019a63458957d3f3d9604ad
Raj-P-Parmar <17comp.raaj.parmar@gmail.com>
2022-01-14 12:13:47 +0530

    Updates

---
+++ precache-manifest.21d13ef968dd8af6561e01cf0d685aec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c0bb87cb4d70b14e5cdf57d1400e715",
+    "url": "/TextUtils/index.html"
+  },
+  {
+    "revision": "32d42c5cf8f6988fa8ab",
+    "url": "/TextUtils/static/js/2.b75a2e40.chunk.js"
+  },
+  {
+    "revision": "a90bb751b102ed9d8f9443a23ededd32",
+    "url": "/TextUtils/static/js/2.b75a2e40.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9be0ff70338fd848b4fa",
+    "url": "/TextUtils/static/js/3.9fbcd7d0.chunk.js"
+  },
+  {
+    "revision": "2343d0d17cac14d0d94e",
+    "url": "/TextUtils/static/js/main.a21ec393.chunk.js"
+  },
+  {
+    "revision": "8fb7dc848dd74c94c2dd",
+    "url": "/TextUtils/static/js/runtime-main.bb60c12c.js"
+  }
+]);
